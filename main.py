8096400f9763a67fb21e5ce9c0f8b9aa7601da80
William <william.lepommelet@gmail.com>
2021-04-11 01:32:09 +0200

    first draft

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def is_at_least_a_10_words_sentence(p) :
+    # sentence needs to be at least 10 words
+    if len(p.split()) < 10 :
+        return False
+    else :
+        return True
+
+def is_not_full_of_number(p) :
+    # sentence needs to be composed by a maximum of 20% digits
+    if sum(c.isdigit() for c in p * 100) / len(p) > 20  :
+        return False
+    else :
+        return True
+
+def is_not_a_wikipedia_recommendation(p) :
+    # sentence isn't part of wikipedia recommendation
+    recommendations = [
+        "Vous pouvez partager vos connaissances",
+        "Si vous disposez d'ouvrages ou d'articles de référence",
+        "Quelles sources sont attendues",
+        "modifier - modifier le code"
+        ]
+    for reco in recommendations :    
+        if reco in p :
+            return False
+    return True
+
+def is_a_good_paragraph(p) :
+    if is_at_least_a_10_words_sentence(p) and is_not_full_of_number(p) and is_not_a_wikipedia_recommendation(p) :
+        return True
+    else :
+        return False
+
+def clean_fact(p):
+    # removing the content between parentheses (whatever)
+    # ISSUE : there's sometimes parenthese within parethese #parentheception #(what(thefuck))
+    # https://fr.wikipedia.org/wiki/Pierre_Oudaille
+    # p=(re.sub(r" ?\([^)]+\)", "", p))
+    # removing notes between square brackets [whatever]
+    p=(re.sub(r" ?\[[^)]+\]", "", p))
+    return p
+
+
+def get_fact_from_random_wiki_page() :
+    response = requests.get('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard')    
+    # response = requests.get('https://fr.wikipedia.org/wiki/Best_~Third_Universe~_/_Universe')
+    # response = requests.get('https://fr.wikipedia.org/wiki/Zaldu')
+    print(response.url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    all_paragraphs = soup.find("div", {"id":"mw-content-text"}).findAll('p')
+    if len(all_paragraphs) != 0 :
+        for p in all_paragraphs:
+            if is_a_good_paragraph(p.text) :
+                return clean_fact(p.text)
+            elif p == all_paragraphs[-1] :                    
+                get_fact_from_random_wiki_page()
+            else :
+                continue
+    else :
+        get_random_wiki_page()
+
+
+
+
+
+print(get_fact_from_random_wiki_page())
---
+++ readme.md
@@ -0,0 +1,3 @@
+## To do
+
+Clean page from useless infobox_vx or bandeau-container
